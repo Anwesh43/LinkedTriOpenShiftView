ce9bd0b038ed715310e8bde52d5b3cfdc31e6023
Anwesh43 <anweshthecool0@gmail.com>
2018-09-09 01:47:24 +0530

    adding drawTOSNode canvas extension method

--- triopenshiftview/src/main/java/com/anwesh/uiprojects/triopenshiftview/TriOpenShiftView.kt
+++ triopenshiftview/src/main/java/com/anwesh/uiprojects/triopenshiftview/TriOpenShiftView.kt
@@ -8,10 +8,38 @@ import android.view.View
 import android.view.MotionEvent
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 
 val nodes : Int = 5
+
+fun Canvas.drawTOSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    paint.color = Color.parseColor("#3949AB")
+    val size : Float = gap/3
+    val sc1 : Float = Math.min(0.5f, scale) * 2
+    val sc2 : Float = Math.min(0.5f, Math.max(scale - 0.5f, 0f)) * 2
+    save()
+    translate(w/2, gap + gap * i)
+    for(j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        save()
+        translate((w/2 - size) , 0f * sc2)
+        rotate(180f * sc2)
+        val path : Path = Path()
+        path.moveTo(0f, -size/2)
+        path.lineTo(size * sc1, 0f)
+        path.lineTo(0f, size/2)
+        drawPath(path, paint)
+        restore()
+        restore()
+    }
+    restore()
+}
 
 class TriOpenShftView(ctx : Context) : View(ctx) {
 
