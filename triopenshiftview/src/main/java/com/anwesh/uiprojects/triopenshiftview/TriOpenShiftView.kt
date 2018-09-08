48769ce046386cca879fd0a23e996d23fe181038
Anwesh43 <anweshthecool0@gmail.com>
2018-09-09 01:31:58 +0530

    adding TriOpenShiftView class

--- triopenshiftview/src/main/java/com/anwesh/uiprojects/triopenshiftview/TriOpenShiftView.kt
+++ triopenshiftview/src/main/java/com/anwesh/uiprojects/triopenshiftview/TriOpenShiftView.kt
@@ -12,3 +12,21 @@ import android.graphics.Paint
 import android.graphics.Path
 
 val nodes : Int = 5
+
+class TriOpenShftView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
