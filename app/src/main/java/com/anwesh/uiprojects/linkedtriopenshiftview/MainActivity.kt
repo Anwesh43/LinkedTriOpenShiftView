47cf3d98b08d6e1c70781f91659915836c5a0582
Anwesh43 <anweshthecool0@gmail.com>
2018-09-09 02:21:13 +0530

    adding View in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtriopenshiftview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtriopenshiftview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedtriopenshiftview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.triopenshiftview.TriOpenShiftView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TriOpenShiftView.create(this)
     }
 }
