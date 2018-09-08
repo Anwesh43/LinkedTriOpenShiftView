74bd31ec036afa8df767245df4e731e514cf3168
Anwesh43 <anweshthecool0@gmail.com>
2018-09-09 01:30:04 +0530

    defining nodes and importing libraries

--- triopenshiftview/src/main/java/com/anwesh/uiprojects/triopenshiftview/TriOpenShiftView.kt
+++ triopenshiftview/src/main/java/com/anwesh/uiprojects/triopenshiftview/TriOpenShiftView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.triopenshiftview
 /**
  * Created by anweshmishra on 09/09/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+
+val nodes : Int = 5
